8b2e4b5f744d9e68af6e1f0fad3f62f3ecaafe18
sander <sjveldhuizen@gmail.com>
2025-03-26 14:41:58 +0100

    omitted cred

--- backstage/yarn-dev.sh
+++ backstage/yarn-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-***REMOVED*** \
-***REMOVED*** \
-***REMOVED*** \
-***REMOVED*** \
+POSTGRES_HOST=backstage-postgres-devplatform0.postgres.database.azure.com \
+POSTGRES_USER=postgresadmin \
+POSTGRES_PORT=5432 \
+POSTGRES_PASSWORD=agi8Ahxat5aopaRi \
 yarn dev
